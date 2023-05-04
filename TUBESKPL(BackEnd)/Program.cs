75d7e2347908141c33a72874be0ba1c3113900dd
tio <tio@TIO>
2023-05-04 10:25:00 +0700

    Add project files.

---
+++ TUBESKPL(BackEnd)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace TUBESKPL_BackEnd_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         PeminjamanService peminjamanService = new PeminjamanService();
+
+            List<String> a = peminjamanService.checkBukuyngAkanDipinjam(4);
+            Console.WriteLine(a[1]);
+
+        }
+    }
+}
